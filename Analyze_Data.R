66adf7a40388faa60248e04f7da13b3cc5bd3818
Calvin Isch <calvin.isch@gmail.com>
2019-06-29 19:29:54 -0400

    Add files via upload

---
+++ Analyze_Data.R
@@ -0,0 +1,8 @@
+# Calvin Isch
+# 6/29/2019
+# Analyzing the data from future thinking experiment
+
+library(ggplot2)
+
+# Import the data:
+all_data <- read.csv("/Users/calvinisch/Desktop/data_for_calvin.csv")
